4a09aeebc1668369adaaa1723bb5faa94ae2dece
Tharanga Wijethilake <dtbwije@gmail.com>
2022-12-25 01:17:19 +0200

    initial commit

---
+++ src/test/kotlin/org/kubernetes/kubernetes/KubernetesApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.kubernetes.kubernetes
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KubernetesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
