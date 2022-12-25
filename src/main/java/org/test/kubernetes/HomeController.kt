6e91e7fc4d0e07f7a521adda03e9f48da2d04218
Tharanga Wijethilake <dtbwije@gmail.com>
2022-12-25 05:03:57 +0200

    REST controller does not work

--- src/main/java/org/test/kubernetes/HomeController.kt
+++ src/main/java/org/test/kubernetes/HomeController.kt
@@ -1,14 +1,11 @@
-package org.test.kubernetes.controller
+package org.test.kubernetes
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
-
 @RestController
 class HomeController {
 
-    @GetMapping("/")
-    fun hello() :String {
-        return "Hello"
-    }
+    @GetMapping("/hello")
+    fun hello() = "Hello"
 }
--- src/main/java/org/test/kubernetes/KubernetesApplication.java
+++ src/main/java/org/test/kubernetes/KubernetesApplication.java
@@ -1,9 +1,13 @@
 package org.test.kubernetes;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "org.test.kubernetes.controller")
 public class KubernetesApplication {
 
 	public static void main(String[] args) {
